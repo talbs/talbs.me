cac6b007d3efd68d2c5d599d31038fe7ec1d8333
Brian Talbot <btalbot@edx.org>
2013-12-18 21:36:06 -0500

    Resume: updating small content, style, and behavioral bits * finalizing client/partner content * adds activities area * adds in more elsewhere copy * adds copy-detail styling * abstracts Sass/CSS - layout, controls, typography further * adding scrolling anchor-based nav * adds xl desktop breakpoint

--- resume/js/main.js
+++ resume/js/main.js
@@ -1,5 +1,35 @@
+function $linkNewWindow(e) {
+  window.open($(e.target).attr('href'));
+  e.preventDefault();
+}
+
+// --------------------
+
 jQuery(document).ready(function($) {
 
+  // removing no-js, accessibility/modernizr marker
   $('html').removeClass('no-js');
 
+  // general link management - new window/tab
+  $('a[data-rel="external"]').bind('click', $linkNewWindow);
+
+  // one page nav - https://github.com/davist11/jQuery-One-Page-Nav
+  $('#nav--global').onePageNav({
+      currentClass: 'is--current',
+      changeHash: false,
+      scrollSpeed: 750,
+      scrollOffset: 30,
+      scrollThreshold: 0.5,
+      filter: '',
+      easing: 'swing',
+      begin: function() {
+          //I get fired when the animation is starting
+      },
+      end: function() {
+          //I get fired when the animation is ending
+      },
+      scrollChange: function($currentListItem) {
+          //I get fired when you enter a section and I pass the list item of the section
+      }
+  });
 });
